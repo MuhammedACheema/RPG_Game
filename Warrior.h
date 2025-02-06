bd98b9457c598aadbd85e53bc987f389311b93dc
Muhammed Cheema <cheemam0807@gmail.com>
2025-02-06 13:43:52 -0500

    just fixed some errors in the warrior class and the character class:

--- Warrior.h
+++ Warrior.h
@@ -5,23 +5,49 @@
 
 class Warrior: public Character {
     public:
+        int stamina;
 
-        Warrior(std::string n){
+        Warrior(std::string n, int stamina){
             this -> name = n;
             this -> health = 150;
             this -> attack = 20;
             this -> defense = 15;
             this -> speed = 10;
+            this -> dodge = false;
+            this -> maxhealth = 150;
+            this -> stamina = stamina;
         }
 
         void specialAbilities(){
             std::cout << name << " has both a power strike doing 50 percent more damage, and shield block providing 5 more defense points" <<std::endl;
         }
 
+        void strike(Character &enemy){
+            if(stamina == 0){
+                std::cout << "you don't have any stamina, please wait for stamina to recharge" << std::endl;
+            }
+            else{
+                std::cout << "You just used powerStrike and did " << attack << " damage, and just used it on "<< enemy.name << std::endl;
+                enemy.takeDamage(attack);
+                stamina -= 1;
+            }
+        }
+
         void powerStrike(Character &enemy){
-            float damage = attack * 1.5;
-            std::cout << "You just used powerStrike and did " << damage << " damage, and just used it on "<< enemy.name << std::endl;
-            enemy.takeDamage(damage);
+            if (stamina < 10 & stamina > 1){
+                std::cout << "you do not have enough stamina, but you just did a regular strike" << std::endl;
+                strike(enemy);
+            }
+            else if(stamina == 0){
+                std::cout << "you don't have any stamina, please wait for stamina to recharge" << std::endl;
+            }
+            else{
+                float damage = attack * 1.5;
+                std::cout << "You just used powerStrike and did " << damage << " damage, and just used it on "<< enemy.name << std::endl;
+                enemy.takeDamage(damage);
+                
+                stamina -= 10;
+            }
         }
 
         void shieldBlock(){
@@ -31,8 +57,18 @@ class Warrior: public Character {
 
         void equipWeapon(std::string sword){
             std::cout << name << ", you have equipped a " << sword << "!" << std::endl;
-            additems(sword);
-            attack += 5;
+            if (std::find(inventory.begin(), inventory.end(), sword) == inventory.end()) {
+                additems(sword);
+                attack += 5;
+            } else {
+                std::cout << sword << " is in the vector." << std::endl;
+            }
+        }
+
+        void regainStamina() {
+            stamina += 5;
+            if (stamina > 30) stamina = 30; // Prevent exceeding max mana
+            std::cout << name << " regenerates 5 Stamina. Current MP: " << stamina << std::endl;
         }
 };
 
